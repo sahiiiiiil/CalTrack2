315e1102d3976dd2ebf3ea8887e223cfc4a312f8
sahiiiiiil <103057807+sahiiiiiil@users.noreply.github.com>
2024-07-18 17:05:26 -0700

    new original version

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View } from 'react-native';
+import MainScreen from './MainScreen';
+import ProfileScreen from './ProfileScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {  return (
+    <View style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="Main" component={MainScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#add8e6', // Light blue background color
+  },
+});
+
+export default App;
